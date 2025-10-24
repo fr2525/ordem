fa658e8692ee3b1ab5f6e75c6c6e762d90d282cb
Fabio Reinert <fr2525@gmail.com>
2025-10-24 10:21:02 -0300

    Aula 23 - Fazendo o processo de adição de usuário - minuto 18

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,14 +1,16 @@
 <?php
 
-defined('BASEPATH') OR exit('Ação não permitida');
+defined('BASEPATH') or exit('Ação não permitida');
 
-class Usuarios extends CI_Controller {
+class Usuarios extends CI_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
-
     }
-    public function index() {
+    public function index()
+    {
 
         $data = array(
 
@@ -25,143 +27,148 @@ class Usuarios extends CI_Controller {
             'usuarios' =>  $this->ion_auth->users()->result(),
         );
 
-       // echo '<pre>';
-       // print_r($data['usuarios']);
-       // exit();
+        // echo '<pre>';
+        // print_r($data['usuarios']);
+        // exit();
 
         $this->load->view('layout/header', $data);
         $this->load->view('usuarios/index');
         $this->load->view('layout/footer');
-
     }
 
-    public function edit($usuario_id = NULL) {
+    public function add()
+    {
 
-        if(!$usuario_id || !$this->ion_auth->user($usuario_id)->row()) {
+        $this->form_validation->set_rules('first_name', '', 'trim|required');
+        $this->form_validation->set_rules('last_name', '', 'trim|required');
+        $this->form_validation->set_rules('email', '', 'trim|required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('username', '', 'trim|required|is_unique[users.username]');
+        $this->form_validation->set_rules('password', 'Senha', 'required|min_length[4]|max_length[100]');
+        $this->form_validation->set_rules('confirm_password', 'Confirme', 'matches[password]');
 
-            //exit('Usuário não encontrado');
-            $this->session->set_flashdata('error','Usuário não encontrado');
-            redirect('usuarios');
 
+        if ($this->form_validation->run()) {
+
+            exit('Validado');
         } else {
 
-/*
-    [first_name] => Admin
-    [last_name] => istrator
-    [email] => admin@admin.com
-    [username] => administrator
-    [active] => 1
-    [perfil_usuario] => 1
-    [password] => 
-    [password1] => 
-    [usuario_id] => 1
-*/
+            // erro de validacao
 
-        $this->form_validation->set_rules('first_name','','trim|required');
-        $this->form_validation->set_rules('last_name','','trim|required');
-        $this->form_validation->set_rules('email','','trim|required|valid_email|callback_email_check[email]');
-        $this->form_validation->set_rules('username','','trim|required|callback_username_check[username]');
-        $this->form_validation->set_rules('password','Senha','min_length[4]|max_length[100]');
-        $this->form_validation->set_rules('password1','Confirma','matches[password]');
-        
-        if($this->form_validation->run()) {
+            $data = array(
+                'titulo' => 'Cadastrar usuário',
 
-
-            $data = elements(
-
-                array(
-                    'first_name',
-                    'last_name',
-                    'email',
-                    'username',
-                    'active',
-                    'password' 
-                   ), $this->input->post()
             );
-
-            $data = $this->security->xss_clean($data);
-
-            $password = $this->input->post('password');
-
-            if (!$password) {
-                unset($data['password']);
-
-            }
-
-            if($this->ion_auth->update($usuario_id, $data))  {
-                
-                $perfil_usuario_db = $this->ion_auth->get_users_groups($usuario_id)->row(); 
-
-                $perfil_usuario_post  = $this->input->post('perfil_usuario');        
-
-                if($perfil_usuario_post != $perfil_usuario_db->id) {
-
-                    $this->ion_auth->remove_from_group($perfil_usuario_db->id, $usuario_id);
-                    $this->ion_auth->add_to_group($perfil_usuario_post, $usuario_id);
-                }
-                
-                $this->session->set_flashdata('sucesso', 'Dados salvos com seucesso');
-
-            } else {
-                 $this->session->set_flashdata('error', 'Erro ao salvar os dados');
-            }
-            redirect('usuarios');
-
-        } else {
-
-             $data = array (
-                'titulo' => 'Editar usuário',
-                'usuario' => $this->ion_auth->user($usuario_id)->row(),
-                'perfil_usuario' =>  $this->ion_auth->get_users_groups($usuario_id)->row(),
-            );
-
-
 
             $this->load->view('layout/header', $data);
-            $this->load->view('usuarios/edit');
+            $this->load->view('usuarios/add');
             $this->load->view('layout/footer');
-
         }
-           
-           
-        }
-
     }
 
-    public function email_check($email) {
+    public function edit($usuario_id = NULL)
+    {
+
+        if (!$usuario_id || !$this->ion_auth->user($usuario_id)->row()) {
+
+            //exit('Usuário não encontrado');
+            $this->session->set_flashdata('error', 'Usuário não encontrado');
+            redirect('usuarios');
+        } else {
+
+            $this->form_validation->set_rules('first_name', '', 'trim|required');
+            $this->form_validation->set_rules('last_name', '', 'trim|required');
+            $this->form_validation->set_rules('email', '', 'trim|required|valid_email|callback_email_check[users.email]');
+            $this->form_validation->set_rules('username', '', 'trim|required|callback_username_check[users.username]');
+            $this->form_validation->set_rules('password', 'Senha', 'min_length[4]|max_length[100]');
+            $this->form_validation->set_rules('confirm_password', 'Confirme', 'matches[password]');
+
+            if ($this->form_validation->run()) {
+
+
+                $data = elements(
+
+                    array(
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'username',
+                        'active',
+                        'password'
+                    ),
+                    $this->input->post()
+                );
+
+                $data = $this->security->xss_clean($data);
+
+                $password = $this->input->post('password');
+
+                if (!$password) {
+                    unset($data['password']);
+                }
+
+                if ($this->ion_auth->update($usuario_id, $data)) {
+
+                    $perfil_usuario_db = $this->ion_auth->get_users_groups($usuario_id)->row();
+
+                    $perfil_usuario_post  = $this->input->post('perfil_usuario');
+
+                    if ($perfil_usuario_post != $perfil_usuario_db->id) {
+
+                        $this->ion_auth->remove_from_group($perfil_usuario_db->id, $usuario_id);
+                        $this->ion_auth->add_to_group($perfil_usuario_post, $usuario_id);
+                    }
+
+                    $this->session->set_flashdata('sucesso', 'Dados salvos com seucesso');
+                } else {
+                    $this->session->set_flashdata('error', 'Erro ao salvar os dados');
+                }
+                redirect('usuarios');
+            } else {
+
+                $data = array(
+                    'titulo' => 'Editar usuário',
+                    'usuario' => $this->ion_auth->user($usuario_id)->row(),
+                    'perfil_usuario' =>  $this->ion_auth->get_users_groups($usuario_id)->row(),
+                );
+
+
+
+                $this->load->view('layout/header', $data);
+                $this->load->view('usuarios/edit');
+                $this->load->view('layout/footer');
+            }
+        }
+    }
+
+    public function email_check($email)
+    {
 
         $usuario_id = $this->input->post('usuario_id');
 
-        if ($this->core_model->get_by_id('users',array('email' => $email , 'id != ' => $usuario_id))) {
+        if ($this->core_model->get_by_id('users', array('email' => $email, 'id != ' => $usuario_id))) {
 
-            $this->form_validation->set_message('email_check','email ja existente.');
+            $this->form_validation->set_message('email_check', 'email ja existente.');
 
             return FALSE;
-
         } else {
 
             return TRUE;
-
         }
-        
     }
 
-    public function username_check($username) {
+    public function username_check($username)
+    {
 
         $usuario_id = $this->input->post('usuario_id');
 
-        if ($this->core_model->get_by_id('users',array('username' => $username , 'id != ' => $usuario_id))) {
+        if ($this->core_model->get_by_id('users', array('username' => $username, 'id != ' => $usuario_id))) {
 
-            $this->form_validation->set_message('username_check','Esse usuario ja existe.');
+            $this->form_validation->set_message('username_check', 'Esse usuario ja existe.');
 
             return FALSE;
-
         } else {
 
             return TRUE;
-
         }
-        
     }
-
 }
--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -50,7 +50,7 @@
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
       <div class="card-header py-3">
-        <a title="Cadastrar novo usuário" href="" class="btn btn-success btn-sm float-right"><i class="fas fa-user-plus"></i>&nbspNovo </a>
+        <a title="Cadastrar novo usuário" href="<?php echo base_url('usuarios/add') ?>" class="btn btn-success btn-sm float-right"><i class="fas fa-user-plus"></i>&nbspNovo </a>
       </div>
       <div class="card-body">
         <div class="table-responsive">
